4b2d61025b301cb7be79c10956fc40cf01e4b269
william rulon <william rulon>
2016-10-07 16:09:09 -0400

    first commit

---
+++ hello.js
@@ -0,0 +1,32 @@
+var builder = require('botbuilder');
+var restify = require('restify');
+
+var connector = new builder.ChatConnector({
+    appId: null,
+    appPassword: null
+});
+
+var bot = new builder.UniversalBot(connector);
+
+
+/*bot.dialog('/', function(session){
+    //session.send('Hello, bot!');
+    var userMessage = session.message.text;
+    session.send('you said ' + userMessage);
+});*/
+
+bot.dialog('/', [
+    function(session) {
+        builder.Prompts.text(session, 'Please enter your name');
+    },
+    function(session, result) {
+        session.send('Hello, ' + result.response);
+    }
+]);
+
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url);
+});
+server.post('/api/messages', connector.listen());
+
