0fb4fcf6d3be7f615d485cfb5cc759be392beca7
william rulon <Rulonwilliam@gmail.com>
2016-10-09 12:27:44 -0400

    added new not bot related thing

--- bot.js
+++ bot.js
@@ -1,50 +1,41 @@
 var builder = require('botbuilder');
 var restify = require('restify');
 
-var connector = new builder.ChatConnector({
-    appId: null,
-    appPassword: null
-});
-
-var bot = new builder.UniversalBot(connector);
-
-bot.dialog('/', new builder.IntentDialog()
-    .matches(/^add/i, '/addTask')
-    .matches(/^change/i, '/changeTask')
-    .matches(/^delete/i, '/deleteTask')
-    .onDefault(builder.DialogAction.send("I'm sorry. I didn't understand"))
-);
-
-/*
-bot.dialog('/', [
-    function (session){
-        session.beginDialog('/phonePrompt');
-    },
-    function (session, result){
-        session.send('Got it... Setting number to %s', results.response);
-    }
-]);
-bot.dialog('/phonePrompt', [
-    function (session, args) {
-       if (args && args.reprompt) {
-         builder.Prompts.text(session, "Enter the number using a format");
-      } else {
-            builder.Prompts.text(session, "what's your phone number?");
-        }
-    },
-    function (session, results) {
-        var matched = results.response.match(/\d+/g);
-        var number = matched ? matched.join('') : '';
-        if (number.length == 10 || number.length == 11) {
-            session.endDialogWithResult({ response: number });
-        } else {
-            session.replaceDialog('/phonePromt', { reprompt: true});
-        }
-    }
-]) */
-
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
     console.log('%s listening to %s', server.name, server.url);
 });
-server.post('/api/messages', connector.listen())
+var connector = new builder.ChatConnector({
+    appId: null,
+    appPassword: null
+});
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+bot.dialog('/', [
+  function (session) {
+    session.beginDialog('/ensureProfile', session,userData.profile);
+  },
+  function (session, result) {
+    session.userData.profile = results.response;
+    session.send('Hello %(name)s! I love %(company)s!', session.userData.profile);
+  }
+]);
+
+bot.dialog('/ensureProfile', [
+  function (session, args, next) {
+    session.dialogData.profile = args || {};
+    if (!session.dialogData.profile.name){
+      builder.Prompts.text(session, "What's your name?");
+    } else {
+      next();
+    }
+  },
+  function (session, result, next) {
+    if (results.response) {
+      session.dialogdata.profile.name = results.response;
+    } else {
+
+    }
+  }
+])
