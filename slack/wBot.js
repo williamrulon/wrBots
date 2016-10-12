8346f7b9b0c793a94180988ab0d3d10397a963f2
Rulonwilliam@gmail.com <Rulonwilliam@gmail.com>
2016-10-12 16:31:33 -0400

    added underground weather api

--- slack/wBot.js
+++ slack/wBot.js
@@ -1,18 +1,18 @@
 var builder = require('botbuilder');
 var restify = require('restify');
+var jQuery = require('jQuery');
 
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
     console.log('%s listening to %s', server.name, server.url);
 });
 var connector = new builder.ChatConnector({
-    appId: null,
-    appPassword: null
+    appId: "84a47283-7f29-492f-b1c7-7d37cccc79fd",
+    appPassword: "MO2jUNoQxdDcWsxz7GksAZn"
 });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
-var weather = "cloudy";
 
 bot.dialog('/', [
   function (session) {
@@ -23,3 +23,16 @@ bot.dialog('/', [
     session.send('The weather is' + weather, session.userData.profile);
   }
 ]);
+var weather = function ({
+  jQuery(document).ready(function($) {
+    $.ajax({
+    url : "http://api.wunderground.com/api/e6e7616a96f73169/geolookup/conditions/q/IA/Cedar_Rapids.json",
+    dataType : "jsonp",
+    success : function(parsed_json) {
+    var location = parsed_json['location']['city'];
+    var temp_f = parsed_json['current_observation']['temp_f'];
+    alert("Current temperature in " + location + " is: " + temp_f);
+    }
+    });
+  });
+});
