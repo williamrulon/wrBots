920093af54e2789c4f2a2a8b6397c129306bafae
Rulonwilliam@gmail.com <Rulonwilliam@gmail.com>
2016-11-17 16:14:01 -0500

    reslising you cnat just run node from a websight

--- app.js
+++ app.js
@@ -1,32 +1,46 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const bot = new Discord.Client();
+//const connect = require('../structures/VoiceChannel');
 
-client.on('ready', () => {
-  console.log('I am ready!');
+bot.on('ready', () => {
+  console.log('I\'m alive!');
 });
 
-const prefix = "..";
+const prefix = ".";
 
-client.on('message', message => {
+bot.on('message', message => {
   if(message.author.bot) return;
   if(!message.content.startsWith(prefix)) return;
 
   if (message.content.startsWith(prefix + 'help')) {
-    message.reply('ty1, fire, daijo');
+    message.channel.sendMessage('Command List:\n- ty1   - crabman\n- fire   - fine\n- daijo\n- kms\n');
   }
-  if (message.content.startsWith(prefix + 'ty1')) {
-    message.reply('https://cdn.discordapp.com/attachments/168173696433782784/243158498479833088/ty1.png');
+  else if (message.content.startsWith(prefix + 'ty1')) {
+    message.channel.sendMessage('https://cdn.discordapp.com/attachments/168173696433782784/243158498479833088/ty1.png');
   }
-  if (message.content.startsWith(prefix + 'fire')) {
-    message.reply('https://cdn.discordapp.com/attachments/168173696433782784/242868911475785729/Jk53KiJ.png');
+  else if (message.content.startsWith(prefix + 'fire')) {
+    message.channel.sendMessage('https://cdn.discordapp.com/attachments/168173696433782784/242868911475785729/Jk53KiJ.png');
   }
-  if (message.content.startsWith(prefix + 'daijo')) {
-    message.reply('https://cdn.discordapp.com/attachments/168173696433782784/242836405179514880/5924519_1bd7b67fd49e1c541bf94a6cb2d996c7.jpg');
+  else if (message.content.startsWith(prefix + 'daijo')) {
+    message.channel.sendMessage('https://cdn.discordapp.com/attachments/168173696433782784/242836405179514880/5924519_1bd7b67fd49e1c541bf94a6cb2d996c7.jpg');
   }
-  if (message.content.startsWith(prefix + 'fine')) {
-    message.reply('https://www.youtube.com/watch?v=5PdXIHGvMpk');
+  else if (message.content.startsWith(prefix + 'kms')) {
+    message.channel.sendMessage(':^)');
   }
-
+  else if (message.content.startsWith(prefix + 'crabman')) {
+    message.channel.sendMessage('https://www.youtube.com/watch?v=voQ38Pvw3Rc');
+  }
+  else if (message.content.startsWith(prefix + 'fine')) {
+      message.channel.sendMessage('https://www.youtube.com/watch?v=5PdXIHGvMpk');
+  }
+  else if (message.content.startsWith(prefix + ' ')) {
+      message.channel.sendMessage('you didn\'t type anything :expressionless:');
+  }
+  else if (message.content.startsWith(prefix + '')) {
+      message.channel.sendMessage('');
+  }
 });
 
-client.login('MjM1NTQ2MzQwMDg2NjQ0NzM4.CwznSw.76pbGExc_GGnQnLXkHXrXerWVtU');
+
+
+bot.login('MjQ3OTAwMDYzOTA3NTc3ODU2.Cw1Gyw.Jl07wEWLioc9EUkwEUES42L-ZMA');
