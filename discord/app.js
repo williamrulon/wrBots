9146a780722219947c2b68c01c6c9b4c22b1d1c4
Rulonwilliam@gmail.com <Rulonwilliam@gmail.com>
2016-12-12 01:04:57 -0500

    small changes and improvments

--- discord/app.js
+++ discord/app.js
@@ -4,14 +4,16 @@ const bot = new Discord.Client();
 bot.on('ready', () => {
   console.log('私は生きている!!!');
 });
+
+//bot.on('');
+
 bot.on('message', message => {
   //this is so the bot can't talk to itself
   if(message.author.bot) return;
-  //this is if the message dont have the items.prefix
   if(!message.content.startsWith(items.prefix)) return;
 
   if (message.content.startsWith(items.prefix + 'help')) {
-    message.channel.sendMessage('Command List:\n- crabman  - daijo\n- ethan   - fire\n- fine  - kermit\n-   - nice\n- nope  - fall\n- waifu  - ???');
+    message.channel.sendMessage('Command List:\n- crabman  - daijo\n- ethan   - fire\n- fine  - kermit\n- nice  - nope\n- fall  - waifu\n- ???');
   }
   else if (message.content.startsWith(items.prefix + 'fire')) {
     message.channel.sendMessage('https://cdn.discordapp.com/attachments/168173696433782784/242868911475785729/Jk53KiJ.png');
@@ -19,8 +21,7 @@ bot.on('message', message => {
   else if (message.content.startsWith(items.prefix + 'daijo')) {
     message.channel.sendMessage('https://cdn.discordapp.com/attachments/168173696433782784/242836405179514880/5924519_1bd7b67fd49e1c541bf94a6cb2d996c7.jpg');
   }
-  // I want the bot to join a call lobby before playing the video
-  // right now its just posting a link to a video I wnt her to play it!!!
+  //I want the bot to join a call lobby before playing the video
   else if (message.content.startsWith(items.prefix + 'crabman')) {
     message.channel.sendMessage('https://www.youtube.com/watch?v=voQ38Pvw3Rc');
   }
@@ -52,8 +53,13 @@ bot.on('message', message => {
     message.channel.sendMessage(item);
   }
   else if (message.content.startsWith(items.prefix + 'waifu')) {
-    let item = items.waifu[Math.floor(Math.random() * items.waifu.length)];
-    message.channel.sendMessage(item);
+    let waifuRole = message.guild.roles.find('name', 'Waifu');
+    if(message.member.roles.has(waifuRole.id)) {
+      let item = items.waifu[Math.floor(Math.random() * items.waifu.length)];
+      message.channel.sendMessage(item);
+    } else {
+      message.reply("Your not a Waifu");
+    }
   }
   else if (message.content.startsWith(items.prefix + 'nice')) {
     message.channel.sendMessage('https://giphy.com/gifs/neonmob-nice-hearts-be-d2YYadXMVk6KhVBe');
@@ -64,8 +70,11 @@ bot.on('message', message => {
   else if (message.content.startsWith(items.prefix + 'chilldino')) {
     message.channel.sendMessage('https://www.youtube.com/playlist?list=PLFA067CBE79DBE316');
   }
+  else if (message.content.startsWith(items.prefix + 'x-mas')) {
+    message.channel.sendMessage('https://33.media.tumblr.com/0dd1e925ac06e979d5b978536ccf1259/tumblr_nx5vuqgN5A1rle9u4o1_500.gif');
+  }
 
 }); // End message handeller
-bot.login(items.token);
+//bot.login(items.token);
 //DaijoBot-^ WrBot-v
-//bot.login('MjM1NTQ2MzQwMDg2NjQ0NzM4.Cyf4zw.YVN5OTiOJdyo2iB7M-MWb1gh3CA');
+bot.login('MjM1NTQ2MzQwMDg2NjQ0NzM4.Cyf4zw.YVN5OTiOJdyo2iB7M-MWb1gh3CA');
